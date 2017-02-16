afec233deb677ca802d55f23fcd07d6cc4cdf812
yanxuegan <yanxuegan0903@163.com>
2017-02-16 16:18:44 +0800

    Initial Commit

---
+++ Speaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Speaker
+//
+//  Created by vsKing on 2017/2/16.
+//  Copyright © 2017年 vsKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
